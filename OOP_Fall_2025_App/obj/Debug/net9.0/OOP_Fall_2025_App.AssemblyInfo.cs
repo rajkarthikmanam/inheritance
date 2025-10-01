a1dc8670109c4d124656d5306117c77cc8a76510
ananyag22 <ananyagp2@gmail.com>
2025-09-30 21:47:25 -0400

    feature/patrons

--- OOP_Fall_2025_App/obj/Debug/net9.0/OOP_Fall_2025_App.AssemblyInfo.cs
+++ OOP_Fall_2025_App/obj/Debug/net9.0/OOP_Fall_2025_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP_Fall_2025_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064a54a72ac5179a250cd1d560092baf03ac2a4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ee1f45ef7fbf79535a75b1d5efaf1af7ff1efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP_Fall_2025_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP_Fall_2025_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
